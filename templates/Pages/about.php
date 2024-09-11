b8b3110d5391d9c67f0fc99537dbe0dd4771f748
iamfinch <iamfinch47@gmail.com>
2024-09-11 05:07:03 -0500

    WIP

---
+++ templates/Pages/about.php
@@ -0,0 +1,71 @@
+<div class="row" style="padding: 15px 0px">
+    <div class="text-center">
+        <h1 class="HomeTitle">Find your best friend!</h1>
+    </div>
+</div>
+
+<div class="row" style="padding: 15px 0px">
+    <div id="HomePageTopLeft" class="col-sm-8">
+        <h2>About the Breeder</h2>
+        <h5>
+            I'm the breeder behind Hannah's Haus & Farm located in Atoka, OK. My husband, I, our dogs live on our 40 acre farm.
+            Our dogs are very loved and well-socialized. I strive to make sure every owner has the ability to get the very best out of their puppy.
+            Our dogs live as part of our family in our home. The puppies will too until they go to their forever home.
+        </h5>
+        <div id="LearnMore">
+            <a class="btn btn-outline-success buttons" href="#Contact">Ask a Question</a>
+        </div>
+
+    </div>
+    <div class="col-sm-4" style="justify-content:right">
+        <?php echo $this->Html->image('cropped_company_logo.png', [
+            'alt' => 'Hannah\'s Haus & Farm Logo',
+            'class' => 'img-fluid w-50 d-block mx-auto LogoPicSize'
+        ]);?>
+    </div>
+</div>
+
+<div class="row" style="padding: 15px 0px">
+    <div id="HomePageTopLeft" class="container">
+        <div class="row">
+            <h2>Why Hannah's Haus & Farm</h2>
+
+            <div class="col-sm-10">
+            <p>
+                Our dogs live with us in our home and we make accommodation for them just as if
+                we make accomodation and upgrades for our selves. Our friends have said they would
+                like to come back as one of our dogs when they pass.
+            </p>
+        </div>
+
+        <div class="col-sm-3 NoPaddingMargins">
+            <h4>A little more about the owner</h4>
+            <p>
+                We have three boys that are grown and not living at home now. Our three dogs are our only
+                kids living at home now. We truly do not understand how people can be complete without dogs as part of the family.
+                Our dogs have been emotional support that helped us get through cancer and the residuals from it now. We
+                want to be a part of transitioning that perfect "pet quality" puppy to the perfect family.
+            </p>
+        </div>
+
+        <div class="col-sm-6 NoPaddingMargins">
+            <?php echo $this->Html->image('homeaboutpic.jpg', [
+                'alt' => 'About us image',
+                'class' => 'img-fluid'
+            ]);?>
+        </div>
+
+        <div class="col-sm-3 NoPaddingMargins">
+            <h4>How we got into breeding</h4>
+            <p>
+                Years ago when we were searching for our own giant
+                schnauzer to bring into our home; we realized how difficult
+                it was finding a giant that was born into a loving well
+                socialized breeders home environment. Our family finally met a fantastic breeder out of state
+                and thats where it all began with Hannah. Our goal is to share the same happiness our giants bring
+                us with others, while making our giants happiness the first priority. We are taking this one litter at time with the
+                advice from our vet and us knowing our own pets and listening to them as well.
+            </p>
+        </div>
+    </div>
+</div>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -19,76 +19,80 @@ $cakeDescription = 'Hannah\'s Haus : ';
 ?>
 <!DOCTYPE html>
 <html>
+    <head>
+        <?= $this->Html->charset() ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>
+            <?= $cakeDescription ?>
+            <?= $this->fetch('title') ?>
+        </title>
+        <?= $this->Html->meta('icon') ?>
 
-<head>
-    <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>
-        <?= $this->fetch('title') ?>
-    </title>
-    <?= $this->Html->meta('icon') ?>
+        <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
+        <?= $this->fetch('meta') ?>
+        <?= $this->fetch('css') ?>
+        <?= $this->fetch('script') ?>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validation-unobtrusive/3.2.12/jquery.validate.unobtrusive.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+            crossorigin="anonymous"
+        ></script>
 
-    <?= $this->fetch('meta') ?>
-    <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validation-unobtrusive/3.2.12/jquery.validate.unobtrusive.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-        crossorigin="anonymous"
-    ></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossorigin="anonymous">
+    </head>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-        crossorigin="anonymous">
-</head>
-
-<body>
-    <div>
-        <div class="top-nav-title">
-            <b><h4 style="margin: 0rem; background-color: #818d97;">Welcome to Hannah's Haus and Farm</h4></b>
+    <body>
+        <div>
+            <div class="top-nav-title">
+                <b><h4 style="margin: 0rem; background-color: #818d97;">Hannah's Haus and Farm</h4></b>
+            </div>
         </div>
-    </div>
 
-    <div style="background-color:#8FACC0; padding: 1rem;">
-        <nav class="top-nav">
-            <div class="top-nav-links">
+        <div style="background-color:#8FACC0; padding: 1rem;">
+            <nav class="top-nav">
+                <div class="top-nav-links">
 
-            <?= $this->Html->link(__('Home'), ['controller' => '/', 'action' => 'index']) ?>
-                <?= $this->Html->link(__('Dog Profiles'), ['controller' => 'dogs', 'action' => 'index']) ?>
+                    <?= $this->Html->link(__('Home'), ['controller' => '/', 'action' => 'index']) ?>
 
-                <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Gallery</a>
+                    <?= $this->Html->link(__('Dogs'), ['controller' => 'dogs', 'action' => 'index']) ?>
 
-                <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Policies</a>
+                    <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Gallery</a>
 
-                <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Privacy</a>
+                    <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Policies</a>
+
+                    <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Privacy</a>
+                </div>
+
+                <div class="top-nav-links">
+                    <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Apply</a>
+                </div>
+            </nav>
+        </div>
+
+        <div style="background-color: #D2E4F1; padding: 1rem; min-height: 100vh; height: 100%;">
+            <main class="main">
+                <div class="container">
+                    <?= $this->Flash->render() ?>
+
+                    <?= $this->fetch('content') ?>
+                </div>
+            </main>
+        </div>
+    </body>
+    <footer style="background-color:#818d97;">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                <?= $this->Html->link(__('About'), ['controller' => 'pages', 'action' => 'about']) ?>
             </div>
-
-            <div class="top-nav-links">
-                <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Apply</a>
-            </div>
-        </nav>
-    </div>
-
-    <div style="background-color: #D2E4F1; padding: 1rem; min-height: 100vh; height: 100%;">
-        <main class="main">
-            <div class="container">
-                <?= $this->Flash->render() ?>
-
-                <?= $this->fetch('content') ?>
-            </div>
-        </main>
-    </div>
-</body>
-<footer style="background-color:#818d97;">
-    test
-</footer>
+        </div>
+    </footer>
 
 </html>
